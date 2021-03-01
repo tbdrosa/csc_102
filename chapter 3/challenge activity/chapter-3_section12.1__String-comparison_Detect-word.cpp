0258d0b492c688d46f2b51f90f0de06378640b69
tbdrosa <tbdrosa@gmail.com>
2021-02-28 23:12:31 -0500

    section12.1 ~String Comparison Detect Word~

---
+++ chapter 3/challenge activity/chapter-3_section12.1__String-comparison_Detect-word.cpp
@@ -0,0 +1,25 @@
+// the main objective/ GOAL:
+// is to Write an if-else statement
+// that prints "Goodbye" if userString is "Quit", else prints "Hello". End with newline.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string userString;
+
+    cin >> userString;
+    /* Your solution goes here  */
+    if (userString == "Quit")
+    {
+        cout << "Goodbye" << endl;
+    }
+    else
+    {
+        cout << "Hello" << endl;
+    }
+
+    return 0;
+}
