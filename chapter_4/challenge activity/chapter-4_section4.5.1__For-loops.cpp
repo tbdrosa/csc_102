32ab72cb3e179dd49aa8661c236f836da25cea5a
tbdrosa <tbdrosa@gmail.com>
2021-03-08 04:21:39 -0500

    section4.5 ~For Loops~

---
+++ chapter_4/challenge activity/chapter-4_section4.5.1__For-loops.cpp
@@ -0,0 +1,26 @@
+// Write code that prints: countNum ... 2 1
+
+// Print a newline after each number.
+
+// Ex: countNum = 3 outputs:
+// 3
+// 2
+// 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int countNum;
+    int i;
+
+    cin >> countNum;
+
+    for (/* Your code goes here */)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
