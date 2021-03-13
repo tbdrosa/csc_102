7ff017d765222468384668cf1a11a71aa9a41eec
tbdrosa <tbdrosa@gmail.com>
2021-03-13 14:56:40 -0500

    section4.11.2 ~Soda Machine with Enums~

---
+++ chapter_4/challenge activity/chapter-4_section4.11.2__Soda-machine-with-enums.cpp
@@ -0,0 +1,52 @@
+// The objective is to Complete the code provided
+// in order to add the appropriate amount to totalDeposit
+
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    enum AcceptedCoins
+    {
+        ADD_QUARTER,
+        ADD_DIME,
+        ADD_NICKEL,
+        ADD_UNKNOWN
+    };
+    int totalDeposit = 0;
+    int userInput;
+
+    totalDeposit = 0;
+
+    cout << "Add coin: 0 (add 25), 1 (add 10), 2 (add 5).  ";
+    cin >> userInput;
+
+    if (userInput == ADD_QUARTER)
+    {
+        totalDeposit = totalDeposit + 25;
+    }
+
+    /* Your solution goes here  */
+    else if (userInput == ADD_DIME)
+    {
+        totalDeposit = totalDeposit + 10;
+    }
+    else if (userInput == ADD_NICKEL)
+    {
+        totalDeposit = totalDeposit + 5;
+    }
+    else if (userInput == ADD_UNKNOWN)
+    {
+        totalDeposit = 0;
+    }
+    else
+    {
+        cout << "Invalid coin selection." << endl;
+    }
+
+    cout << "totalDeposit: " << totalDeposit << endl;
+
+    return 0;
+}
