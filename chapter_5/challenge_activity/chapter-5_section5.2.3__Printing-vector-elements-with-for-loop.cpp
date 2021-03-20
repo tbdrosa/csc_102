bf986abf3c5e447db2a72152c8bb0f93312777a3
tbdrosa <tbdrosa@gmail.com>
2021-03-19 21:41:26 -0400

    section5.2.3 ~Printing Vector Elements with a for Loop~

---
+++ chapter_5/challenge_activity/chapter-5_section5.2.3__Printing-vector-elements-with-for-loop.cpp
@@ -0,0 +1,53 @@
+// Write a for loop to print all NUM_VALS elements of vector courseGrades,
+// following each with a space (including the last). Print forwards, then backwards.
+//End with newline.
+
+//Ex: If courseGrades = {7, 9, 11, 10},
+
+// print:
+// 7 9 11 10
+//10 11 9 7
+
+//Hint: Use two for loops. Second loop starts with i = courseGrades.size() - 1 (Notes)
+
+//Note: These activities may test code with different test values.
+// This activity will perform two tests,
+// both with a 4-element vector (vector<int> courseGrades(4)).
+//See "How to Use zyBooks".
+
+//Also note: If the submitted code tries to access an invalid vector element,
+// such as courseGrades.at(9) for a 4-element vector,
+// the test may generate strange results.
+// Or the test may crash and report "Program end never reached",
+// in which case the system doesn't print the test case that caused the reported message.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    const int NUM_VALS = 4;
+    vector<int> courseGrades(NUM_VALS);
+    int i;
+
+    for (i = 0; i < courseGrades.size(); ++i)
+    {
+        cin >> courseGrades.at(i);
+    }
+
+    /* Your solution goes here  */
+
+    for (i = 0; i < NUM_VALS; i++)
+    {
+        cout << courseGrades[i] << " ";
+    }
+    cout << endl;
+    for (i = NUM_VALS - 1; i >= 0; i--)
+    {
+        cout << courseGrades[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
