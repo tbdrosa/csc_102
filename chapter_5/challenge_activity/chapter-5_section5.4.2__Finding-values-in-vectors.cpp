58767a674552bf21405dcf246911603f3f4f5dfd
tbdrosa <tbdrosa@gmail.com>
2021-03-20 19:49:29 -0400

    section5.4.2 ~Finding Values in Vectors~

---
+++ chapter_5/challenge_activity/chapter-5_section5.4.2__Finding-values-in-vectors.cpp
@@ -0,0 +1,43 @@
+// The goal of this challenge activity is to
+// assign numMatches with the number of elements in userValues that equal matchValue. u
+//serValues has NUM_VALS elements.
+
+//Ex: If userValues is {2, 2, 1, 2} and matchValue is 2 ,
+//then numMatches should be 3.
+
+// Your code will be tested with the following values:
+// * matchValue: 2, userValues: {2, 2, 1, 2} (as in the example program above)
+// * matchValue: 0, userValues: {0, 0, 0, 0}
+// * matchValue: 50, userValues: {10, 20, 30, 40}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    const int NUM_VALS = 4;
+    int matchValue;
+    unsigned int i;
+    int numMatches = -99; // Assign numMatches with 0 before your for loop
+    vector<int> userValues(NUM_VALS);
+
+    cin >> matchValue;
+
+    for (i = 0; i < userValues.size(); ++i)
+    {
+        cin >> userValues.at(i);
+    }
+
+    /* Your solution goes here  */
+    numMatches = 0;
+    for (i = 0; i < NUM_VALS; ++i)
+    {
+        if (userValues[i] == matchValue)
+            ++numMatches;
+    }
+
+    cout << "matchValue: " << matchValue << ", numMatches: " << numMatches << endl;
+
+    return 0;
+}
