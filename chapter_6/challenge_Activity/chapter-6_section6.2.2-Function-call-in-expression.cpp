41ce78937e3fb9936d04e3c3ca25271437743ec1
tbdrosa <tbdrosa@gmail.com>
2021-04-09 13:03:01 -0400

    section6.2.2 ~Function Call in Expression~

---
+++ chapter_6/challenge_Activity/chapter-6_section6.2.2-Function-call-in-expression.cpp
@@ -0,0 +1,50 @@
+// The goal of this challenge activity is to
+// Assign to maxSum the max of(numA, numB) PLUS the max of(numY, numZ).
+
+// Use just one statement.
+
+// Hint : Call FindMax() twice in an expression.
+
+#include <iostream>
+using namespace std;
+
+double FindMax(double num1, double num2)
+{
+    double maxVal;
+
+    // Note: if-else statements need not be understood to complete this activity
+    if (num1 > num2)
+    {                  // if num1 is greater than num2,
+        maxVal = num1; // then num1 is the maxVal.
+    }
+    else
+    {                  // Otherwise,
+        maxVal = num2; // num2 is the maxVal.
+    }
+
+    return maxVal;
+}
+
+int main()
+{
+    double numA;
+    double numB;
+    double numY;
+    double numZ;
+    double maxSum;
+
+    cin >> numA;
+    cin >> numB;
+    cin >> numY;
+    cin >> numZ;
+
+    double maxAorB = FindMax(numA, numB);
+    double maxYorZ = FindMax(numY, numZ);
+
+    // Adding the 2 values and storing it in maxSum variable
+    maxSum = maxAorB + maxYorZ;
+
+    cout << "maxSum is: " << maxSum << endl;
+
+    return 0;
+}
