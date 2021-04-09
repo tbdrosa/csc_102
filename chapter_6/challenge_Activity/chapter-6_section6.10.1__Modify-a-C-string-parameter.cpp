2b072efc8856d4c2bcf0c12f827603a7ec20b7a9
tbdrosa <tbdrosa@gmail.com>
2021-04-09 14:18:08 -0400

    section6.10.1 ~Modify a C String Parameter~

---
+++ chapter_6/challenge_Activity/chapter-6_section6.10.1__Modify-a-C-string-parameter.cpp
@@ -0,0 +1,32 @@
+// The goal of this challenge activity is to Complete the function
+// in order to replace any period by an exclamation point.
+
+// Ex: "Hello. I'm Miley. Nice to meet you." becomes:
+// "Hello! I'm Miley! Nice to meet you!"
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void MakeSentenceExcited(char *sentenceText)
+{
+
+    /* Your solution goes here  */
+    for (int i = 0; i < 50; i++)
+    {
+        if (sentenceText[i] == '.')
+            sentenceText[i] = '!';
+    }
+}
+
+int main()
+{
+    const int TEST_STR_SIZE = 50;
+    char testStr[TEST_STR_SIZE];
+
+    cin.getline(testStr, TEST_STR_SIZE);
+    MakeSentenceExcited(testStr);
+    cout << testStr << endl;
+
+    return 0;
+}
